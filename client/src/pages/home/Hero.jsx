0c91fc0e662e72db857439c05549e4685c64921a
Aryan Tyagi 27 <mraryanttyagiji@gmail.com>
2025-06-28 13:27:53 +0530

    Video blur

--- client/src/pages/home/Hero.jsx
+++ client/src/pages/home/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
       <section className="relative overflow-hidden lg:flex h-[30vh] sm:h-[30vh] lg:h-screen lg:items-center">
 
         {/* Video Background */}
-        <div className="absolute inset-0 w-full h-full overflow-hidden">
+        {/* <div className="absolute inset-0 w-full h-full overflow-hidden">
           <video
             className="w-full h-full object-cover pointer-events-none"
             // src="https://cdn.pixabay.com/video/2024/02/05/199341-910144573_large.mp4"
@@ -24,12 +24,28 @@ function Hero() {
             muted
             playsInline
           />
+        </div> */}
+        <div className="absolute inset-0 w-full h-full overflow-hidden">
+
+          {/* Original Video */}
+          <video
+            className="w-full h-full object-cover pointer-events-none"
+            src={heroVideo}
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
+
+          {/* Blurred Overlay */}
+          <div className="absolute inset-0 w-full h-full pointer-events-none blur-overlay"></div>
+
         </div>
 
         {/* Animated Text Content */}
         <div className="z-10 absolute mx-auto max-w-screen-xl lg:px-24 px-4 sm:px-6 top-[50%] -translate-y-[50%]">
-          
-          <motion.div 
+
+          <motion.div
             className="max-w-2xl text-center sm:text-left group"
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
@@ -41,7 +57,7 @@ function Hero() {
               initial={{ opacity: 0, x: -50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ delay: 0.5, duration: 1 }}
-             
+
             >
               Kisan <strong className="font-bold text-rose-700">Bazaar</strong>
             </motion.h1>
@@ -51,7 +67,7 @@ function Hero() {
               initial={{ opacity: 0, x: 50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ delay: 0.8, duration: 1 }}
-             
+
             >
               Connecting Farmers and Consumers - Bringing Fresh Produce to Your Doorstep!
             </motion.p>

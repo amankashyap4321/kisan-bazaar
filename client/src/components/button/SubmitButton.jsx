16e0acaa568c5635f98e1302b3a72a5ab06621d7
Aryan Tyagi 27 <mraryanttyagiji@gmail.com>
2025-06-28 13:41:05 +0530

    Button color improved

--- client/src/components/button/SubmitButton.jsx
+++ client/src/components/button/SubmitButton.jsx
@@ -1,11 +1,30 @@
+// import React from "react";
+// import Spinner from "../loading/Spinner";
+
+// const SubmitButton = ({ bgColor, hoverBgColor, text, isLoading }) => {
+//   return (
+//     <button
+//       type="submit"
+//       className={`w-full flex justify-center items-center text-white ${bgColor} hover:${hoverBgColor} font-medium rounded-lg text-sm px-5 py-2.5 text-center`}
+//     >
+//       {isLoading && <Spinner width="w-5" color="#ffffff" />}
+//       {text}
+//     </button>
+//   );
+// };
+
+// export default SubmitButton;
+
+
 import React from "react";
 import Spinner from "../loading/Spinner";
 
-const SubmitButton = ({ bgColor, hoverBgColor, text, isLoading }) => {
+const SubmitButton = ({ text, isLoading }) => {
   return (
     <button
       type="submit"
-      className={`w-full flex justify-center items-center text-white ${bgColor} hover:${hoverBgColor} font-medium rounded-lg text-sm px-5 py-2.5 text-center`}
+      className="w-full flex justify-center items-center font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+      bg-blue-600 text-white hover:bg-white hover:text-blue-600 border border-blue-600 transition-all"
     >
       {isLoading && <Spinner width="w-5" color="#ffffff" />}
       {text}
